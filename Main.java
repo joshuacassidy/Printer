64c46be32aeda396522e9813a053e97481da5438
joshuacassidy <Joshcassidy79@gmail.com>
2017-07-16 23:04:36 +0100

    Cleaned up the random methods for the input by using random class and randomising the print mode

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args){
         System.out.print("\033[H\033[2J");
         Random random = new Random();
-        Printer firstPrinter = new Printer((int)(Math.random()*50),false);
-        print(firstPrinter,firstPrinter,(int)(Math.random()*20),(int)(Math.random()*500));
+        Printer firstPrinter = new Printer(random.nextInt(50)+1,random.nextBoolean());
+        print(firstPrinter,firstPrinter,random.nextInt(20)+1,random.nextInt(500)+1);
 
-        Printer secondPrinter = new Printer((int)(Math.random()*55),true);
-        print(secondPrinter,secondPrinter,(int)(Math.random()*22),(int)(Math.random()*10000));
+        Printer secondPrinter = new Printer(random.nextInt(55)+1,random.nextBoolean());
+        print(secondPrinter,secondPrinter,random.nextInt(22)+1,random.nextInt(10000)+502);
     }
 
     public static void print(Printer jobNumber, Printer getPagesPrinted,int pages,int printerId){
